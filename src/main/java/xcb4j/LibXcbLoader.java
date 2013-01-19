d3bca4bd0e061a12c15ec74ab692fba3b83d6ab4
Erik De Rijcke <DeRijcke.Erik@gmail.com>
2013-01-19 23:25:33 +0100

    changed the way the native lib is loaded

--- src/main/java/xcb4j/LibXcbLoader.java
+++ src/main/java/xcb4j/LibXcbLoader.java
@@ -1,9 +1,10 @@
 package xcb4j;
 
 import java.io.File;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class LibXcbLoader {
 
@@ -13,15 +14,27 @@ public class LibXcbLoader {
 		final String jreArch = System.getProperty("os.arch");
 		final String libName = LibXcbLoader.LIBXCB_BASE_NAME + "_" + jreArch + ".so";
 
-		final URL libUrl = LibXcbLoader.class.getClassLoader().getResource(libName);
+		InputStream in = LibXcbLoader.class.getClassLoader().getResourceAsStream(libName);
+		byte[] buffer = new byte[4096];
+		int read = -1;
+		File temp = new File(	new File(System.getProperty("java.io.tmpdir")),
+								libName);
+		FileOutputStream fos;
 		try {
-			final String path = new File(new URI(libUrl.toString())).getAbsolutePath();
+			fos = new FileOutputStream(temp);
+			while ((read = in.read(buffer)) != -1) {
+				fos.write(	buffer,
+							0,
+							read);
+			}
+			fos.close();
+			in.close();
 
-			System.load(path);
-		} catch (final URISyntaxException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.load(temp.getAbsolutePath());
+		} catch (FileNotFoundException e) {
+			throw new Error(e);
+		} catch (IOException e) {
+			throw new Error(e);
 		}
-
 	}
 }
